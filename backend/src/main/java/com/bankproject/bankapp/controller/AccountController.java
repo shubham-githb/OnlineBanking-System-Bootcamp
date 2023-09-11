2f63469dba0621a641ff8dc4d1b3b14deed136b6
Praneel Jimmy <prajim18@gmail.com>
2023-09-11 08:05:16 +0530

    Added code for Account and Transaction Controllers

--- backend/src/main/java/com/bankproject/bankapp/controller/AccountController.java
+++ backend/src/main/java/com/bankproject/bankapp/controller/AccountController.java
@@ -1,4 +1,40 @@
 package com.bankproject.bankapp.controller;
 
+import com.bankproject.bankapp.entity.Account;
+import com.bankproject.bankapp.service.AccountService;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/account")
 public class AccountController {
+    @Autowired
+    AccountService AccountService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Account> getAccount(@PathVariable Long id) {
+        return new ResponseEntity<>(AccountService.getAccount(id), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<Account> saveAccount(@RequestBody Account account) {
+        return new ResponseEntity<>(AccountService.saveAccount(account), HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<HttpStatus> deleteAccount(@PathVariable Long id) {
+        AccountService.deleteAccount(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<Account>> getAccounts() {
+        return new ResponseEntity<>(AccountService.getAccounts(), HttpStatus.OK);
+    }
 }
--- backend/src/main/java/com/bankproject/bankapp/controller/TransactionController.java
+++ backend/src/main/java/com/bankproject/bankapp/controller/TransactionController.java
@@ -1,4 +1,36 @@
 package com.bankproject.bankapp.controller;
 
+import com.bankproject.bankapp.entity.Transaction;
+import com.bankproject.bankapp.service.TransactionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
 public class TransactionController {
+    @Autowired
+    TransactionService TransactionService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Transaction> getTransaction(@PathVariable Long id) {
+        return new ResponseEntity<>(TransactionService.getTransaction(id), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<Transaction> saveTransaction(@RequestBody Transaction transaction) {
+        return new ResponseEntity<>(TransactionService.saveTransaction(transaction), HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<HttpStatus> deleteTransaction(@PathVariable Long id) {
+        TransactionService.deleteTransaction(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<Transaction>> getTransactions() {
+        return new ResponseEntity<>(TransactionService.getTransactions(), HttpStatus.OK);
+    }
 }
